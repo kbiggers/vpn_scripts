ff1cac0ea5889d76329b42777fedfb4d7fc58b22
Kevin <krbiggers@gmail.com>
2019-03-23 20:05:06 -0400

    WIP: changes to vpn scripts

--- pia_action.py
+++ pia_action.py
@@ -1,3 +1,4 @@
+##TODO add checking to make sure it can connect to PIA
 import os
 import requests
 import subprocess
@@ -9,11 +10,11 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 class PiaAction:
-    def __init__():
-        self.config_path = '/etc/openvpn/client/'
+    def __init__(self):
+        self.configpath = '/etc/openvpn/client/'
 
     def select_connection(self):
-        files = os.listdir(configpath)
+        files = os.listdir(self.configpath)
         conn_dict = {}     
         i = 0                         
         for file in files: 
@@ -22,14 +23,16 @@ class PiaAction:
                 i+=1
         print(conn_dict)
         connection = conn_dict[int(input('Select Connection: '))]
-        PiaAction.connect_pia(connection,configpath)
+        PiaAction.connect_pia(connection,self.configpath)
 
-    def connect_pia(connection,configpath):
+    def connect_pia(self,connection):
         LOG.info('Attempting to connect to PIA...')
+        configpath = self.configpath
         vpn = subprocess.Popen("/usr/bin/sudo /usr/sbin/openvpn --cd {confdir} --config {confdir}{connection}".format(confdir=configpath, connection=connection
 ), shell=True, stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         pid = vpn.pid 
 
+    @staticmethod 
     def disconnect_pia():
         LOG.info('Disconnecting PIA VPN')
         command = subprocess.run(["/usr/bin/sudo", "/usr/bin/pkill", "openvpn"],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -39,6 +42,7 @@ class PiaAction:
         else:
             LOG.info("PIA Killed!: {}".format(command.sterr))
     
+    @staticmethod
     def check_pia():
         LOG.info('Checking status of PIA VPN')
         adapters = []
@@ -50,7 +54,7 @@ class PiaAction:
             return True
             LOG.info('PIA is running!')
         else:
+            LOG.warning('PIA is NOT running')
             return False
-            LOG.warning('PIA is NOT running, killing rtorrent...')
 
         
--- watch_tunnel.py
+++ watch_tunnel.py
@@ -1,2 +1,36 @@
-#TODO use this to create a watcher job that will kill rtorrent it it notices it running without the VPN running
+import logging
+import os
+import subprocess
 
+from pia_action import PiaAction
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+class Watcher:
+    def __init__(self, process):
+        self.process = process 
+        self.pid = self.get_pid()
+
+    def get_pid(self):
+        command = subprocess.run(['/usr/bin/sudo','/bin/pidof',"{}".format(self.process)],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        if command.returncode != 0:
+            log.info("{} is not running".format(self.process))
+        else:
+            return command.stdout.decode("utf-8").rstrip()
+
+    def kill_process(self):
+        LOG.info("killing {}...".format(self.process))
+        command = subprocess.run(["/usr/bin/sudo", "/bin/kill","-9", "{}".format(self.pid)],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        if command.returncode != 0:
+            LOG.error("{} not killed".format(self.process))
+            LOG.error(command.stderr.decode('utf-8'))
+        else:
+            LOG.info("{} Killed!: {}".format(self.process, command.stderr.decode('utf-8')))
+
+    def watch(self):
+        pia = PiaAction()
+        if pia.check_pia() == False and self.get_pid(): 
+            LOG.info("{} is running without vpn tunnel, killing PID {}".format(self.process, self.pid))
+            self.kill_process()
+        
